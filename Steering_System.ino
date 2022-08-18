4ff1e0183b19ef40ba9cf52558d936e9c2dc20ad
Ian.waiguru <67041860+Iann-urus@users.noreply.github.com>
2022-08-18 18:46:15 +0300

    Update Steering_System.ino

--- Steering_System.ino
+++ Steering_System.ino
@@ -1,44 +1,61 @@
 #include <Servo.h>
 #include <NewPing.h>
 //Ultrasonic sensor
-#define Trig_pin2 11//Right
-#define Echo_pin2 10
-#define max_dist 400
-/*
-A low gain makes the system sluggish and unresponsive. 
-A gain that is too high makes the axis oscillate or vibrate.
-*/
-#define Kp 180/20 //Proportional Gain
-//This value is for the right trajectory relative to the Sonar sensor
-#define desired_dist 5
+  #define Sensors 2
+  #define max_dist 400
+  NewPing sonar[Sensors] = {    //Create an array to loop through the sensor readings
+    NewPing(10, 11, max_dist),
+    NewPing(8, 9, max_dist)
+  };
+//Create global variables
+int Left,Right,D_fe,Sx,Current_Trajectory,KD_fe = 0;
+int Kpe = 0;//Desired Trajectory to balance on both sides,also Kpe  
 //Create an object for the servo
-int servoPin = 9;
+#define servoPin 5
 Servo servo;
-//Create an object for the Ultrasonic sensor
-NewPing sonar(Trig_pin2,Echo_pin2,max_dist);
-
 void setup() {
     servo.attach(servoPin); 
     Serial.begin(115200);   
 }
-
 void loop() {
-/*
- * Calculate Steering angle
- * (180/20(Kp) * Cross Track Error(cm)(Ep)) =Steering Angle
- */
-int actual_dist = sonar.ping_cm();
-int Ep = (actual_dist - desired_dist);//Calculate Cross Track Error 
-int steering_angle = ((Kp) * Ep);
-if( actual_dist <= 5){
-  steering_angle= 0;
- }
-if(actual_dist >= 20){
-  steering_angle = 180;
+  getKpe();
+  normal_mode();
+  Serial.print(",");
+  Serial.print(Left);
+  Serial.print(",");
+  Serial.print(Right);
+  Serial.print(",Servo Angle:");
+  Serial.print(Sx);
+  Serial.print(",D_fe");
+  Serial.print(D_fe);
+  Serial.print(",Kpe:");
+  Serial.println(Kpe);
+}  
+void getKpe(){
+  delay(50);
+  Left = sonar[0].ping_cm();
+  Right = sonar[1].ping_cm();
+   D_fe =(Left + Right);
+   KD_fe =((Left + Right)/2);
+   if(Left > Right){
+    Current_Trajectory = Left;
+   }
+   if(Right > Left){
+    Current_Trajectory = Right;
+   }
+   if(Current_Trajectory == Left){
+    Serial.print("T:");
+    Serial.print(Current_Trajectory);
+   }
+   if(Current_Trajectory == Right){
+    Serial.print("F:");
+    Serial.print(Current_Trajectory);
+   }
+   Kpe = (Current_Trajectory-KD_fe);//Desired Trajectory to balance on both sides,also Kpe  
 }
-servo.write(steering_angle);
-Serial.print(actual_dist);
-Serial.print(',');
-Serial.println(steering_angle);
-delay(50);    
-}
+void normal_mode(){
+  int Mx = 180;
+  int Kp = (Mx/D_fe);
+  Sx = (Kp * Kpe);
+  servo.write(Sx);
+  }
